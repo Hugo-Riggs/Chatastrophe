a36775a4d37983bd6492772fd3d0b49d10258fd4
HugoRiggs <hsplatas@gmail.com>
2017-03-07 01:21:32 -0500

    client with printlns

--- src/main/scala/RequestReply/Client.scala
+++ src/main/scala/RequestReply/Client.scala
@@ -27,24 +27,31 @@ class Client(remote: InetSocketAddress, listener: ActorRef) extends Actor {
 
   def receive = {
     case CommandFailed(_: Connect) =>
+      println("connect failed")
       listener ! "connect failed"
       context stop self
 
     case c @ Connected(remote, local) =>
+      println("connected " + c.toString)
       listener ! c
       val connection = sender()
       connection ! Register(self)
       context become {
         case data: ByteString =>
+          println(data.decodeString("UTF-8"))
           connection ! Write(data)
         case CommandFailed(w: Write) =>
+          println("write failed")
           // O/S buffer was full
           listener ! "write failed"
         case Received(data) =>
+          println(data.decodeString("UTF-8"))
           listener ! data
         case "close" =>
+          println("closing connection")
           connection ! Close
         case _: ConnectionClosed =>
+          println("conection was closed")
           listener ! "connection closed"
           context stop self
       }
